2dd26ba4002fc75a5ce08483685c9e58bf4cc1fa
ardipflix <vakarchuk.dmytryi@armar.solutions>
2024-09-23 12:52:22 +0300

    save

--- lib/src/repository/repository.dart
+++ lib/src/repository/repository.dart
@@ -2,6 +2,6 @@ import 'package:apical/src/native_apical_client.dart';
 import 'package:dio/dio.dart';
 import 'package:get_it/get_it.dart';
 
-sealed class Repository {
+base class Repository {
   Dio get client => GetIt.instance.get<NativeApicalClient>();
 }
